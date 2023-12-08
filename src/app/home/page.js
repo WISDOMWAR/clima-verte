128f536be03c9d8390637f2ac27557d8023bdd21
Mohamedkhalilkhelifi <mohamedkhalilkhelifi69@gmail.com>
2023-12-08 04:29:35 +0100

    updates

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -28,6 +28,9 @@ export default function Page() {
             </div>
             <Store/>
             <Reclamation/>
+            <div>
+                
+            </div>
         </div>
     )
 }
